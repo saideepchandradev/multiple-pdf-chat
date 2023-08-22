d6f4c2a5f477a617ca821292d0a837e64f3aadac
saideepchandradev <saideepchandra@outlook.com>
2023-08-22 16:28:38 +0530

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
-st.secrets["OPENAI_API_KEY"]
-st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+key = st.secrets["OPENAI_API_KEY"]
+token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
 def get_pdf_text(pdf_docs):
     text = ""
